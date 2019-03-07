b4f92a062ce925eb2cd90c533d51e9680f3c06f0
Vincent Boutour <bob@vibioh.fr>
2019-03-07 15:11:58 +0100

    Updates

---
+++ precache-manifest.68d8e5dd53a9424cc4e05a8db1e54420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65ccf8e9767902c00f83",
+    "url": "/react-mapbox-wrapper/static/js/runtime~main.c41b8163.js"
+  },
+  {
+    "revision": "a8ffe237bc38f1d54a96",
+    "url": "/react-mapbox-wrapper/static/js/main.d52e145c.chunk.js"
+  },
+  {
+    "revision": "df4a06320828e78cb30a",
+    "url": "/react-mapbox-wrapper/static/js/2.523bd8f4.chunk.js"
+  },
+  {
+    "revision": "a8ffe237bc38f1d54a96",
+    "url": "/react-mapbox-wrapper/static/css/main.f679fef0.chunk.css"
+  },
+  {
+    "revision": "1929c21031301ec46a6a1c67e51b4300",
+    "url": "/react-mapbox-wrapper/index.html"
+  }
+];
