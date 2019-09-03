8e2c400cb38d401b00d9512ade3b04bd812ffa45
Simon Robin <simon.robin.info@gmail.com>
2019-09-03 11:48:55 +0200

    add sentry to try it on examples website

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({dsn: "https://250287b3c5ac41809f8823a96707acca@sentry.io/1549967"});
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
